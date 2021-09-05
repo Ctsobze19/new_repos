d0c7fb1ef6b17891a636959697dff6b504dfe79d
Ctsobze19 <47582888+Ctsobze19@users.noreply.github.com>
2021-09-05 08:46:42 -0400

    Add files via upload

---
+++ Find_method.py
@@ -0,0 +1,16 @@
+word = 'apples'
+if "pp" in word[:3]:
+    print('A pp is in here')
+else:
+     print('There is no pp in here')
+
+words = 'An application of anumerical methods used within python libraries are known to return previous iterations of drastic mechanical unclearness'
+
+sentence = words.startswith('An')
+
+if sentence is True:
+    print("good job python")
+
+location = words.find("")
+
+print(sentence)
